1f9ec69509fa7f9d66bfa09aa30adc31e3a2a2de
Oliver Steele <steele@osteele.com>
2021-07-27 14:15:37 +0800

    Hook commands up to server

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,26 @@
 import * as vscode from 'vscode';
+import { Server } from 'p5-server';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "p5-server" is now active!');
+  console.log('Congratulations, your extension "p5-server" is now active!');
+  let server: Server = null;
 
-	let disposable = vscode.commands.registerCommand('p5-server.start', () => {
-		vscode.window.showInformationMessage('p5-server is running!');
-	});
-	context.subscriptions.push(disposable);
-	disposable = vscode.commands.registerCommand('p5-server.stop', () => {
-		vscode.window.showInformationMessage('p5-server is stopped!');
-	});
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(vscode.commands.registerCommand('p5-server.start', () => {
+    const wsFolders = vscode.workspace?.workspaceFolders;
+    const wsPath = wsFolders ? wsFolders[0].uri.fsPath : '.';
+    console.log(`Starting server at ${wsPath}`);
+    server?.stop();
+    server = new Server({ root: wsPath }).start((url: string) => {
+      vscode.window.showInformationMessage(`p5-server is at ${url}`);
+    });
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand('p5-server.stop', () => {
+    console.log('Stopping server');
+    server?.stop();
+    server = null;
+    vscode.window.showInformationMessage('p5-server is stopped!');
+  }));
 }
 
 export function deactivate() { }
