4eda1206c0d62c33f56c990062859a971713f727
Oliver Steele <steele@osteele.com>
2021-08-07 13:08:03 +0800

    Format w/ prettier

--- src/extension.ts
+++ src/extension.ts
@@ -8,17 +8,25 @@ import { SketchTreeProvider } from './sketchExplorer';
 export function activate(context: vscode.ExtensionContext) {
   let servicesAvailable = false;
   let server: Server | null;
-  let state: "stopped" | "starting" | "running" | "stopping" = "stopped";
+  let state: 'stopped' | 'starting' | 'running' | 'stopping' = 'stopped';
   let wsPath: string | undefined;
 
-  const rootPath = (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0)
-    ? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
+  const rootPath =
+    vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
+      ? vscode.workspace.workspaceFolders[0].uri.fsPath
+      : undefined;
   const sketchTreeProvider = new SketchTreeProvider(rootPath);
   vscode.window.registerTreeDataProvider('p5sketchExplorer', sketchTreeProvider);
 
-  context.subscriptions.push(vscode.commands.registerCommand('p5-explorer.refresh', () => sketchTreeProvider.refresh()));
-  context.subscriptions.push(vscode.commands.registerCommand('p5-server.createSketchFile', createSketch.bind(null, false)));
-  context.subscriptions.push(vscode.commands.registerCommand('p5-server.createSketchFolder', createSketch.bind(null, true)));
+  context.subscriptions.push(
+    vscode.commands.registerCommand('p5-explorer.refresh', () => sketchTreeProvider.refresh())
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand('p5-server.createSketchFile', createSketch.bind(null, false))
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand('p5-server.createSketchFolder', createSketch.bind(null, true))
+  );
 
   if (!vscode.workspace.workspaceFolders) {
     return;
@@ -35,17 +43,21 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(vscode.commands.registerCommand('p5-server.start', startServer));
   context.subscriptions.push(vscode.commands.registerCommand('p5-server.stop', stopServer));
-  context.subscriptions.push(vscode.commands.registerCommand('p5-server.openInBrowser', () => {
-    const editorPath = window.activeTextEditor?.document.fileName;
-    vscode.commands.executeCommand('p5-server.openBrowser', editorPath ? Uri.file(editorPath) : undefined);
-  }));
-  context.subscriptions.push(vscode.commands.registerCommand('p5-server.openBrowser', (uri?: Uri) => {
-    if (state === 'stopped') {
-      startServer(uri);
-    } else if (state === 'running') {
-      openBrowser(uri);
-    }
-  }));
+  context.subscriptions.push(
+    vscode.commands.registerCommand('p5-server.openInBrowser', () => {
+      const editorPath = window.activeTextEditor?.document.fileName;
+      vscode.commands.executeCommand('p5-server.openBrowser', editorPath ? Uri.file(editorPath) : undefined);
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand('p5-server.openBrowser', (uri?: Uri) => {
+      if (state === 'stopped') {
+        startServer(uri);
+      } else if (state === 'running') {
+        openBrowser(uri);
+      }
+    })
+  );
   context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(updateFromConfiguration));
 
   updateFromConfiguration();
@@ -59,26 +71,27 @@ export function activate(context: vscode.ExtensionContext) {
 
   function updateStatusBarItems() {
     switch (state) {
-      case 'running': {
-        const browserName = vscode.workspace.getConfiguration('p5-server').get<string>('browser', 'default');
-        statusBarServerItem.text = "$(extensions-star-full)P5 Server";
-        statusBarServerItem.tooltip = "Stop the P5 server";
-        statusBarServerItem.command = 'p5-server.stop';
-        statusBarOpenItem.tooltip = `Open ${server!.url} in the ${browserName} browser`;
-      }
+      case 'running':
+        {
+          const browserName = vscode.workspace.getConfiguration('p5-server').get<string>('browser', 'default');
+          statusBarServerItem.text = '$(extensions-star-full)P5 Server';
+          statusBarServerItem.tooltip = 'Stop the P5 server';
+          statusBarServerItem.command = 'p5-server.stop';
+          statusBarOpenItem.tooltip = `Open ${server!.url} in the ${browserName} browser`;
+        }
         break;
       case 'stopped':
-        statusBarServerItem.text = "$(extensions-star-empty)P5 Server";
-        statusBarServerItem.tooltip = "Click to start the P5 server";
+        statusBarServerItem.text = '$(extensions-star-empty)P5 Server';
+        statusBarServerItem.tooltip = 'Click to start the P5 server';
         statusBarServerItem.command = 'p5-server.start';
         break;
       case 'starting':
-        statusBarServerItem.text = "$(extensions-star-full~spin)P5 Server";
-        statusBarServerItem.tooltip = "The P5 server is starting…";
+        statusBarServerItem.text = '$(extensions-star-full~spin)P5 Server';
+        statusBarServerItem.tooltip = 'The P5 server is starting…';
         break;
       case 'stopping':
-        statusBarServerItem.text = "$(extensions-star-empty~spin)P5 Server";
-        statusBarServerItem.tooltip = "The P5 server is stopping";
+        statusBarServerItem.text = '$(extensions-star-empty~spin)P5 Server';
+        statusBarServerItem.tooltip = 'The P5 server is stopping';
         break;
     }
     const config = vscode.workspace.getConfiguration('p5-server');
@@ -95,25 +108,30 @@ export function activate(context: vscode.ExtensionContext) {
   }
 
   async function startServer(uri?: Uri) {
-    if (state !== 'stopped') { return; }
+    if (state !== 'stopped') {
+      return;
+    }
     state = 'starting';
 
     try {
       const wsFolders = vscode.workspace?.workspaceFolders?.map(f => f.uri.fsPath) || [];
       // TODO: select the folder that contains uri
-      wsPath = wsFolders.length > 1
-        ? await vscode.window.showQuickPick(
-          wsFolders,
-          { placeHolder: 'Select a folder to serve' })
-        : (wsFolders[0] || '.');
-      if (!wsPath) { return; }
+      wsPath =
+        wsFolders.length > 1
+          ? await vscode.window.showQuickPick(wsFolders, {
+              placeHolder: 'Select a folder to serve'
+            })
+          : wsFolders[0] || '.';
+      if (!wsPath) {
+        return;
+      }
 
       server?.stop();
       server = null;
 
       let sbm = vscode.window.setStatusBarMessage(`Starting the P5 server at ${wsPath}`);
 
-      statusBarServerItem.text = "$(extensions-star-empty) P5 Server";
+      statusBarServerItem.text = '$(extensions-star-empty) P5 Server';
       server = new Server({ root: wsPath });
       await server.start();
       state = 'running';
@@ -131,11 +149,15 @@ export function activate(context: vscode.ExtensionContext) {
   }
 
   function stopServer() {
-    if (!server) { return; }
-    if (state !== 'running') { return; }
+    if (!server) {
+      return;
+    }
+    if (state !== 'running') {
+      return;
+    }
     state = 'stopping';
 
-    let sbm = vscode.window.setStatusBarMessage("Shutting down the P5 server…");
+    let sbm = vscode.window.setStatusBarMessage('Shutting down the P5 server…');
 
     server.stop();
     server = null;
@@ -150,18 +172,21 @@ export function activate(context: vscode.ExtensionContext) {
 
   async function createSketch(folder: boolean) {
     if (!servicesAvailable) {
-      window.showErrorMessage("You must have at least one folder open to create a sketch.");
+      window.showErrorMessage('You must have at least one folder open to create a sketch.');
     }
 
-    let sketchName = await vscode.window
-      .showInputBox({
-        value: ``,
-        prompt: `Enter the name of the p5.js sketch`,
-        ignoreFocusOut: true,
-      });
-    if (!sketchName) { return; }
+    let sketchName = await vscode.window.showInputBox({
+      value: ``,
+      prompt: `Enter the name of the p5.js sketch`,
+      ignoreFocusOut: true
+    });
+    if (!sketchName) {
+      return;
+    }
     sketchName = sketchName.trim();
-    if (sketchName.length === 0) { return; }
+    if (sketchName.length === 0) {
+      return;
+    }
     if (!folder && !sketchName.endsWith('.js')) {
       sketchName += '.js';
     }
@@ -173,7 +198,10 @@ export function activate(context: vscode.ExtensionContext) {
     const dirPath = path.dirname(filePath);
     const basePath = path.basename(sketchName);
     const sketch = folder
-      ? Sketch.create(path.join(filePath, 'index.html'), { scriptPath: 'sketch.js', title: sketchName })
+      ? Sketch.create(path.join(filePath, 'index.html'), {
+          scriptPath: 'sketch.js',
+          title: sketchName
+        })
       : Sketch.create(path.join(dirPath, basePath));
 
     try {
@@ -186,7 +214,7 @@ export function activate(context: vscode.ExtensionContext) {
 
     vscode.window.showTextDocument(Uri.file(path.join(sketch.dirPath, sketch.scriptPath)));
     vscode.commands.executeCommand('p5-explorer.refresh');
-    vscode.commands.executeCommand("revealInExplorer", dirPath);
+    vscode.commands.executeCommand('revealInExplorer', dirPath);
   }
 
   async function openBrowser(uri?: Uri) {
@@ -214,7 +242,7 @@ export function activate(context: vscode.ExtensionContext) {
       await new Promise(resolve => setTimeout(resolve, 1000));
     }
     if (process.exitCode !== 0) {
-      let msg = "The browser failed to open.";
+      let msg = 'The browser failed to open.';
       if (browserKey !== 'default') {
         msg += ` The ${browserName} browser may not be available on your system.`;
       }
@@ -223,4 +251,4 @@ export function activate(context: vscode.ExtensionContext) {
   }
 }
 
-export function deactivate() { }
+export function deactivate() {}
--- src/sketchExplorer.ts
+++ src/sketchExplorer.ts
@@ -33,8 +33,11 @@ export class SketchTreeProvider implements vscode.TreeDataProvider<SketchItem | 
     if (element instanceof DirectoryItem) {
       return Promise.resolve(this.getDirectoryChildren(element.filePath));
     } else if (element instanceof SketchItem) {
-      return Promise.resolve(element.sketch.files.map(f =>
-        new FileItem(path.join(element.sketch.dirPath, f), vscode.TreeItemCollapsibleState.None)));
+      return Promise.resolve(
+        element.sketch.files.map(
+          f => new FileItem(path.join(element.sketch.dirPath, f), vscode.TreeItemCollapsibleState.None)
+        )
+      );
     } else {
       return Promise.resolve(this.getDirectoryChildren(this.workspaceRoot));
     }
@@ -42,17 +45,24 @@ export class SketchTreeProvider implements vscode.TreeDataProvider<SketchItem | 
 
   private getDirectoryChildren(dir: string) {
     const exclusions = ['.*', 'node_modules', 'package.json'];
-    const { sketches, unaffiliatedFiles } = Sketch.analyzeDirectory(dir, { exclusions });
+    const { sketches, unaffiliatedFiles } = Sketch.analyzeDirectory(dir, {
+      exclusions
+    });
     const files = unaffiliatedFiles.map(s => path.join(dir, s));
     return [
-      ...sketches.map(sketch =>
-        new SketchItem(sketch, sketch.files.length > 1 ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None)),
+      ...sketches.map(
+        sketch =>
+          new SketchItem(
+            sketch,
+            sketch.files.length > 1 ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None
+          )
+      ),
       ...files
         .filter(s => fs.statSync(s).isDirectory())
         .map(s => new DirectoryItem(s, vscode.TreeItemCollapsibleState.Collapsed)),
       ...files
         .filter(s => !fs.statSync(s).isDirectory())
-        .map(s => new FileItem(s, vscode.TreeItemCollapsibleState.None)),
+        .map(s => new FileItem(s, vscode.TreeItemCollapsibleState.None))
     ];
   }
 }
@@ -62,14 +72,15 @@ interface FilePathItem {
 }
 
 class SketchItem extends vscode.TreeItem implements FilePathItem {
-  constructor(
-    public readonly sketch: Sketch,
-    public readonly collapsibleState: vscode.TreeItemCollapsibleState
-  ) {
+  constructor(public readonly sketch: Sketch, public readonly collapsibleState: vscode.TreeItemCollapsibleState) {
     super(sketch.name.replace(/\/$/, ''), collapsibleState);
     this.tooltip = `p5.js sketch at ${this.filePath}`;
     this.description = sketch.description;
-    this.command = { command: 'p5-explorer.openSketch', title: "Edit P5.js Sketch", arguments: [sketch] };
+    this.command = {
+      command: 'p5-explorer.openSketch',
+      title: 'Edit P5.js Sketch',
+      arguments: [sketch]
+    };
   }
 
   get filePath() {
@@ -79,16 +90,13 @@ class SketchItem extends vscode.TreeItem implements FilePathItem {
 
   iconPath = {
     dark: path.join(__filename, '..', '..', 'resources', 'dark', 'sketch.svg'),
-    light: path.join(__filename, '..', '..', 'resources', 'light', 'sketch.svg'),
+    light: path.join(__filename, '..', '..', 'resources', 'light', 'sketch.svg')
   };
   contextValue = 'sketch';
 }
 
 class DirectoryItem extends vscode.TreeItem implements FilePathItem {
-  constructor(
-    public readonly filePath: string,
-    public readonly collapsibleState: vscode.TreeItemCollapsibleState
-  ) {
+  constructor(public readonly filePath: string, public readonly collapsibleState: vscode.TreeItemCollapsibleState) {
     super(path.basename(filePath), collapsibleState);
     this.tooltip = `${this.label}`;
   }
@@ -98,18 +106,23 @@ class DirectoryItem extends vscode.TreeItem implements FilePathItem {
 }
 
 class FileItem extends vscode.TreeItem implements FilePathItem {
-  constructor(
-    public readonly filePath: string,
-    public readonly collapsibleState: vscode.TreeItemCollapsibleState
-  ) {
+  constructor(public readonly filePath: string, public readonly collapsibleState: vscode.TreeItemCollapsibleState) {
     super(path.basename(filePath), collapsibleState);
     this.tooltip = filePath;
-    this.command = { command: 'vscode.open', title: "Edit File", arguments: [vscode.Uri.file(filePath)] };
+    this.command = {
+      command: 'vscode.open',
+      title: 'Edit File',
+      arguments: [vscode.Uri.file(filePath)]
+    };
     this.iconPath = new vscode.ThemeIcon(
-      /\.(gif|jpe?g|png|svg|wav|mp3|mov|mp4)/i.test(filePath) ? 'file-media'
-        : /\.css|html|js?$/i.test(filePath) ? 'file-code'
-          : /\.text$/i.test(filePath) ? 'file-text'
-            : 'file');
+      /\.(gif|jpe?g|png|svg|wav|mp3|mov|mp4)/i.test(filePath)
+        ? 'file-media'
+        : /\.css|html|js?$/i.test(filePath)
+        ? 'file-code'
+        : /\.text$/i.test(filePath)
+        ? 'file-text'
+        : 'file'
+    );
   }
   contextValue = 'file';
 }
