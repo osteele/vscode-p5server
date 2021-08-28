752642ed91bb75c30384e9ee02f314e3c162828d
Oliver Steele <steele@osteele.com>
2021-08-28 15:47:27 +0800

    Copy sketches that aren't in their own directories

--- src/sketchExplorer.ts
+++ src/sketchExplorer.ts
@@ -16,17 +16,41 @@ export class SketchTreeProvider implements vscode.TreeDataProvider<FilePathItem 
   }
 
   private registerCommands() {
-    commands.registerCommand('p5-explorer.duplicateSelectedSketch', async (sketch: Sketch) => {
+    commands.registerCommand('p5-explorer.duplicateSketch', async (sketch: Sketch) => {
       if (!(sketch instanceof Sketch)) throw new Error(`${sketch} is not a Sketch`);
+      let name = await window.showInputBox();
+      if (!name) return;
+      name = name.replace(/-?\s+-?/g, '-');
       // if the sketch is the only item in the directory, copy the directory
       const { sketches } = await Sketch.analyzeDirectory(sketch.dir, { exclusions });
-      if (sketches.length !== 1 || sketches[0].mainFile !== sketch.mainFile) {
-        return vscode.window.showErrorMessage('Cannot copy a sketch that is not the only item in the directory');
+      if (sketches.length === 1 && sketches[0].mainFile === sketch.mainFile) {
+        await workspace.fs.copy(Uri.file(sketch.dir), Uri.file(path.join(path.dirname(sketch.dir), name)));
+        this.refresh();
+      } else {
+        // copy the sketch files within the same directory
+        const basename = name.replace(/\.(js|html?)$/i, '');
+        const replacements = [sketch.mainFile, sketch.scriptFile]
+          .filter(Boolean)
+          .map(file => ({
+            src: Uri.file(path.join(sketch.dir, file)),
+            target: Uri.file(path.join(sketch.dir, basename + path.extname(file)))
+          }))
+          .map(rec => ({ ...rec, targetExists: fsExists(rec.target) }));
+        const targetsExist = await Promise.all(replacements.map(({ target }) => fsExists(target)));
+        if (targetsExist.some(Boolean)) {
+          const target = replacements[targetsExist.indexOf(true)].target;
+          window.showErrorMessage(`Can't create a duplicate named ${name}. ${target.fsPath} already exists`);
+          return;
+        }
+        await Promise.all(replacements.map(({ src, target }) => workspace.fs.copy(src, target)));
+        this.refresh();
       }
-      const name = await window.showInputBox();
-      if (!name) return;
-      await workspace.fs.copy(Uri.file(sketch.dir), Uri.file(path.join(path.dirname(sketch.dir), name)));
-      this.refresh();
+      async function fsExists(uri: Uri): Promise<boolean> {
+        return workspace.fs.stat(uri).then(
+          () => true,
+          () => false
+        );
+      }
     });
     commands.registerCommand('p5-explorer.createFolder', async (item: DirectoryItem) => {
       if (!(item instanceof DirectoryItem)) throw new Error(`${item} is not a directory`);
